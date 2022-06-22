9bf464daac633f4e827ecd5c4e64cb855020f45d
Caio Faraleski <caiotadeu_2012@hotmail.com>
2022-06-22 18:39:30 -0300

    creating components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from './components/Header';
+import { SideBar } from './components/SideBar';
+import { Video } from './components/Video';
+import { Lesson } from './components/Lesson';
+import './styles/global.css'
+import { Event } from './pages/Event';
+
+function App() {
+  return (
+    <Event/>
+  );
+}
+
+export default App
