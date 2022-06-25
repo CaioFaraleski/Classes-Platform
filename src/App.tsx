a18f2c4344b109344bbbbeb8474bcb81695fc76d
Caio Faraleski <caiotadeu_2012@hotmail.com>
2022-06-25 15:40:34 -0300

    fix the route problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './styles/global.css'
 import { ApolloProvider } from '@apollo/client';
 import { client } from './lib/apollo';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Router } from './Router';
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <BrowserRouter>
+      <HashRouter>
         <Router />
-      </BrowserRouter>
+      </HashRouter>
     </ApolloProvider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'classes-platform',
   plugins: [react()]
 })
